0a714ed2f9eb9dc8a7b00684de63f441b1bd1ebd
Thomas Peitz <tsupertramp90@gmail.com>
2018-04-25 11:34:53 +0200

    Add config file parsing

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	_ "encoding/json"
 	"flag"
-	_ "fmt"
+	"fmt"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log"
@@ -11,46 +11,21 @@ import (
 	_ "strings"
 )
 
-type Config struct {
-	checks []Check
-}
-
-type Tag struct {
-	key   string
-	value string
-}
-
-type Metric struct {
-	name       string
-	statistics string
-	period     int
-}
-
-type Check struct {
-	name    string
-	tags    []Tag
-	metrics []Metric
-}
-
 var (
 	addr       = flag.String("listen-address", ":5000", "The address to listen on.")
 	configFile = flag.String("config.file", "config.yml", "Path to configuration file.")
 )
 
 func metricsHandler(w http.ResponseWriter, req *http.Request) {
-
-	//yamlParse := readConfig
-	tag := Tag{key: "Name", value: "test-name"}
-	tags := []Tag{tag}
-	metric := Metric{name: "CPUUtilization", statistics: "Average", period: 60}
-	metrics := []Metric{metric}
-	check := Check{name: "jenkins", tags: tags, metrics: metrics}
-	checks := []Check{check}
-	//config := Config{checks: checks}
+	var c conf
+	c.getConf(configFile)
 
 	registry := prometheus.NewRegistry()
-	prometheus.DefaultGatherer = registry
-	ec2Metrics(registry, checks)
+
+	//ec2Metrics(registry, checks, labelConfig)
+	//elbMetrics(registry, checks, labelConfig)
+
+	prometheus.DefaultGatherer = registry //- Do i need this? :D
 
 	handler := promhttp.HandlerFor(registry, promhttp.HandlerOpts{
 		DisableCompression: false,
@@ -62,13 +37,14 @@ func metricsHandler(w http.ResponseWriter, req *http.Request) {
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(`<html>
-            <head><title>Prometheus Cloudwatch Exporter</title></head>
+            <head><title>Yet another cloudwatch exporter</title></head>
             <body>
-            <h1>Prometheus Cloudwatch Exporter</h1>
+            <h1>Exporter with benefits</h1>
             <p><a href="/metrics">Metrics</a></p>
             </body>
             </html>`))
 	})
+
 	flag.Parse()
 	http.HandleFunc("/metrics", metricsHandler)
 	log.Fatal(http.ListenAndServe(*addr, nil))
