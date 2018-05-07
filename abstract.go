7369dfe79c636903598263aef36cb877778500b0
Thomas Peitz <tsupertramp90@gmail.com>
2018-05-07 16:52:22 +0200

    Remove PrometheusexportedTags function

--- abstract.go
+++ abstract.go
@@ -10,42 +10,19 @@ type awsResources struct {
 }
 
 type awsResource struct {
-	Id         *string
-	Tags       []*tag
-	Service    *string
-	Attributes map[string]*string
+	Id      *string
+	Tags    []*tag
+	Service *string
 }
 
-func createPrometheusExportedTags(jobs []job) map[string][]string {
-	exportedTags := map[string]map[string]bool{
-		"ec":  map[string]bool{},
-		"ec2": map[string]bool{},
-		"elb": map[string]bool{},
-		"es":  map[string]bool{},
-		"rds": map[string]bool{},
-	}
-
-	output := map[string][]string{
-		"ec":  []string{},
-		"ec2": []string{},
-		"elb": []string{},
-		"es":  []string{},
-		"rds": []string{},
-	}
-
-	for _, job := range jobs {
-		for _, tag := range job.Discovery.ExportedTags {
-			exportedTags[job.Discovery.Type][tag] = true
-		}
-	}
-
-	for k, v := range exportedTags {
-		for kk, _ := range v {
-			output[k] = append(output[k], kk)
-		}
-	}
-
-	return output
+type cloudwatchData struct {
+	Id         *string
+	Metric     *string
+	Service    *string
+	Statistics *string
+	Value      *float64
+	Empty      bool
+	Tags       []*tag
 }
 
 func (r awsResource) filterThroughTags(filterTags []tag) bool {
