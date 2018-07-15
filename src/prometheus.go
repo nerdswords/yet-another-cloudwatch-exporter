b6f9d177b52e3e14d69286a2908c91f2f9d9fb68
Thomas Peitz <tsupertramp90@gmail.com>
2018-07-15 22:50:58 +0200

    Finish draft of metrics data

--- src/prometheus.go
+++ src/prometheus.go
@@ -4,82 +4,39 @@ import (
 	_ "fmt"
 	"github.com/prometheus/client_golang/prometheus"
 	"strings"
-	"sync/atomic"
+	_ "sync/atomic"
 )
 
-func createPrometheusMetrics(resources []*awsInfoData, cloudwatch []*cloudwatchData) *prometheus.Registry {
+type PrometheusData struct {
+	name   *string
+	labels map[string]string
+	value  *float64
+}
+
+func createPrometheusMetrics(p PrometheusData) *prometheus.Gauge {
+	gauge := prometheus.NewGauge(prometheus.GaugeOpts{
+		Name:        *p.name,
+		Help:        "Help is not implemented yet.",
+		ConstLabels: p.labels,
+	})
+
+	gauge.Set(*p.value)
+
+	return &gauge
+}
+
+func fillRegistry(promData []*PrometheusData) *prometheus.Registry {
 	registry := prometheus.NewRegistry()
 
-	exportedTags := findExportedTags(resources)
-
-	pushCounter := prometheus.NewCounter(prometheus.CounterOpts{
-		Name: "yace_cloudwatch_requests_total",
-		Help: "Help is not implemented yet.",
-	})
-	pushCounter.Set(float64(atomic.LoadUint64(&CloudwatchApiRequests)))
-
-	registry.MustRegister(pushCounter)
-
-	for _, r := range resources {
-		metric := createInfoMetric(r, exportedTags[*r.Service])
-		registry.MustRegister(metric)
-	}
-
-	for _, c := range cloudwatch {
-		if c.Value != nil {
-			metric := createCloudwatchMetric(*c)
-			registry.MustRegister(metric)
-		}
+	for _, point := range promData {
+		gauge := createPrometheusMetrics(*point)
+		registry.MustRegister(*gauge)
 	}
 
 	return registry
 }
 
-func createCloudwatchMetric(data cloudwatchData) prometheus.Gauge {
-	labels := prometheus.Labels{
-		"name": *data.Id,
-	}
-
-	name := "aws_" + strings.ToLower(*data.Service) + "_" + strings.ToLower(promString(*data.Metric)) + "_" + strings.ToLower(promString(*data.Statistics))
-
-	gauge := prometheus.NewGauge(prometheus.GaugeOpts{
-		Name:        name,
-		Help:        "Help is not implemented yet.",
-		ConstLabels: labels,
-	})
-
-	gauge.Set(*data.Value)
-
-	return gauge
-}
-
-func createInfoMetric(resource *awsInfoData, exportedTags []string) prometheus.Gauge {
-	promLabels := make(map[string]string)
-
-	promLabels["name"] = *resource.Id
-
-	name := "aws_" + *resource.Service + "_info"
-
-	for _, exportedTag := range exportedTags {
-		escapedKey := "tag_" + promString(exportedTag)
-		promLabels[escapedKey] = ""
-		for _, resourceTag := range resource.Tags {
-			if exportedTag == resourceTag.Key {
-				promLabels[escapedKey] = resourceTag.Value
-			}
-		}
-	}
-
-	gauge := prometheus.NewGauge(prometheus.GaugeOpts{
-		Name:        name,
-		Help:        "Help is not implemented yet.",
-		ConstLabels: promLabels,
-	})
-
-	return gauge
-}
-
-func promString(text string) string {
+func PromString(text string) string {
 	replacer := strings.NewReplacer(" ", "_", ",", "_", "\t", "_", ",", "_", "/", "_", "\\", "_", ".", "_", "-", "_")
 	return replacer.Replace(text)
 }
