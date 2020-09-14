647b12ad2e5f24a946f8a540e6669a3034988a57
Rob Russo <robert.russo@hbo.com>
2020-09-14 09:43:00 -0400

    label consistency (#235)
    * label recording function
    
    * disable inconsistency
    
    * ensure consistent metric labels
    
    * restore describe
    
    * remove debug output
    
    * formatting

--- aws_cloudwatch.go
+++ aws_cloudwatch.go
@@ -43,6 +43,8 @@ type cloudwatchData struct {
 	Period                  int64
 }
 
+var labelMap = make(map[string][]string)
+
 func createCloudwatchSession(region *string, roleArn string) *cloudwatch.CloudWatch {
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
 		SharedConfigState: session.SharedConfigEnable,
@@ -182,7 +184,6 @@ func createListMetricsOutput(dimensions []*cloudwatch.Dimension, namespace *stri
 func dimensionsToCliString(dimensions []*cloudwatch.Dimension) (output string) {
 	for _, dim := range dimensions {
 		output = output + "Name=" + *dim.Name + ",Value=" + *dim.Value
-		fmt.Println(output)
 	}
 	return output
 }
@@ -608,11 +609,6 @@ func createPrometheusLabels(cwd *cloudwatchData) map[string]string {
 	switch *cwd.Service {
 	case "sfn":
 		labels["dimension_"+promStringTag("StateMachineArn")] = getStateMachineNameFromArn(*cwd.ID)
-	case "apigateway":
-		// The same dimensions are required on all metrics by prometheus
-		for _, key := range []string{"Stage", "Resource", "Method"} {
-			labels["dimension_"+promStringTag(key)] = ""
-		}
 	}
 
 	for _, dimension := range cwd.Dimensions {
@@ -627,6 +623,49 @@ func createPrometheusLabels(cwd *cloudwatchData) map[string]string {
 	}
 
 	return labels
+}
+
+func recordLabelsForMetric(metricName string, promLabels map[string]string) {
+	var workingLabelsCopy []string
+	if _, ok := labelMap[metricName]; ok {
+		copy(labelMap[metricName], workingLabelsCopy)
+	}
+
+	for k, _ := range promLabels {
+		workingLabelsCopy = append(workingLabelsCopy, k)
+	}
+	sort.Strings(workingLabelsCopy)
+	j := 0
+	for i := 1; i < len(workingLabelsCopy); i++ {
+		if workingLabelsCopy[j] == workingLabelsCopy[i] {
+			continue
+		}
+		j++
+		workingLabelsCopy[j] = workingLabelsCopy[i]
+	}
+	labelMap[metricName] = workingLabelsCopy[:j+1]
+}
+
+func ensureLabelConsistencyForMetrics(metrics []*PrometheusMetric) []*PrometheusMetric {
+	var updatedMetrics []*PrometheusMetric
+
+	for _, prometheusMetric := range metrics {
+		metricName := prometheusMetric.name
+		metricLabels := prometheusMetric.labels
+
+		consistentMetricLabels := make(map[string]string)
+
+		for _, recordedLabel := range labelMap[*metricName] {
+			if value, ok := metricLabels[recordedLabel]; ok {
+				consistentMetricLabels[recordedLabel] = value
+			} else {
+				consistentMetricLabels[recordedLabel] = ""
+			}
+		}
+		prometheusMetric.labels = consistentMetricLabels
+		updatedMetrics = append(updatedMetrics, prometheusMetric)
+	}
+	return updatedMetrics
 }
 
 func sortByTimestamp(datapoints []*cloudwatch.Datapoint) []*cloudwatch.Datapoint {
@@ -703,9 +742,12 @@ func migrateCloudwatchToPrometheus(cwd []*cloudwatchData) []*PrometheusMetric {
 			serviceName := fixServiceName(c.Service, c.Dimensions)
 			name := "aws_" + serviceName + "_" + strings.ToLower(promString(*c.Metric)) + "_" + strings.ToLower(promString(statistic))
 			if exportedDatapoint != nil {
+
+				promLabels := createPrometheusLabels(c)
+				recordLabelsForMetric(name, promLabels)
 				p := PrometheusMetric{
 					name:             &name,
-					labels:           createPrometheusLabels(c),
+					labels:           promLabels,
 					value:            exportedDatapoint,
 					timestamp:        timestamp,
 					includeTimestamp: includeTimestamp,
