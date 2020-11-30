6f520229ac9bf607b77ba470d4a3dab1b5a03401
Aaron Murray <aaron@aaron-murray.net>
2020-11-30 01:00:08 -0800

    Fix sorting of datapoints, add test util functions (#267)
    Fix test
    
    Co-authored-by: Aaron Murray <aamurray@cisco.com>

--- aws_cloudwatch_test.go
+++ aws_cloudwatch_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/aws/aws-sdk-go/service/cloudwatch"
 )
@@ -40,4 +41,36 @@ func TestGetNamespace(t *testing.T) {
 	if ns != "" {
 		t.Fatalf("jobType foobar should have returned empty string")
 	}
+}
+
+// TestSortyByTimeStamp validates that sortByTimestamp() sorts in descending order.
+func TestSortyByTimeStamp(t *testing.T) {
+	cloudWatchDataPoints := make([]*cloudwatch.Datapoint, 3)
+	maxValue1 := float64(1)
+	maxValue2 := float64(2)
+	maxValue3 := float64(3)
+
+	dataPointMiddle := &cloudwatch.Datapoint{}
+	twoMinutesAgo := time.Now().Add(time.Minute * 2 * -1)
+	dataPointMiddle.Timestamp = &twoMinutesAgo
+	dataPointMiddle.Maximum = &maxValue2
+	cloudWatchDataPoints[0] = dataPointMiddle
+
+	dataPointNewest := &cloudwatch.Datapoint{}
+	oneMinutesAgo := time.Now().Add(time.Minute * -1)
+	dataPointNewest.Timestamp = &oneMinutesAgo
+	dataPointNewest.Maximum = &maxValue1
+	cloudWatchDataPoints[1] = dataPointNewest
+
+	dataPointOldest := &cloudwatch.Datapoint{}
+	threeMinutesAgo := time.Now().Add(time.Minute * 3 * -1)
+	dataPointOldest.Timestamp = &threeMinutesAgo
+	dataPointOldest.Maximum = &maxValue3
+	cloudWatchDataPoints[2] = dataPointOldest
+
+	sortedDataPoints := sortByTimestamp(cloudWatchDataPoints)
+
+	equals(t, maxValue1, *sortedDataPoints[0].Maximum)
+	equals(t, maxValue2, *sortedDataPoints[1].Maximum)
+	equals(t, maxValue3, *sortedDataPoints[2].Maximum)
 }
