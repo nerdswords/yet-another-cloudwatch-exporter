55801ba4715a8896b38c0d5f541f2c0022503598
Juha Ylitalo <juha.ylitalo@reaktor.com>
2021-06-10 15:44:13 +0300

    [cmd] Drop 'exporter' from import line

--- cmd/yace/main.go
+++ cmd/yace/main.go
@@ -11,7 +11,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	log "github.com/sirupsen/logrus"
 
-	exporter "github.com/ivx/yet-another-cloudwatch-exporter/pkg"
+	"github.com/ivx/yet-another-cloudwatch-exporter/pkg"
 )
 
 var version = "custom-build"
