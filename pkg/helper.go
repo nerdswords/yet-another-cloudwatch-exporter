6b371dfa3655c95e3cdab07b45c81bc22bc62fc4
amerlin <antoniomerlin@devhawkstudio.com>
2021-09-28 13:28:41 +0530

    Add support for loading env if present

--- pkg/helper.go
+++ pkg/helper.go
@@ -1,5 +1,10 @@
 package exporter
 
+import (
+	"os"
+	"strconv"
+)
+
 func stringInSlice(str string, list []string) bool {
 	for _, v := range list {
 		if v == str {
@@ -7,4 +12,39 @@ func stringInSlice(str string, list []string) bool {
 		}
 	}
 	return false
+}
+
+func GetEnv(key string, defaultValue interface{}) interface{} {
+	val, ok := os.LookupEnv(key)
+	switch key {
+	case "debug":
+		if ok {
+			value, _ := strconv.ParseBool(val)
+			return value
+		} else {
+			return defaultValue
+		}
+	case "addr":
+		if ok {
+			return val
+		} else {
+			return defaultValue
+		}
+	case "scrapingInterval":
+		if ok {
+			value, _ := strconv.Atoi(val)
+			return value
+		} else {
+			return defaultValue
+		}
+	case "metricsPerQuery":
+		if ok {
+			value, _ := strconv.Atoi(val)
+			return value
+		} else {
+			return defaultValue
+		}
+	default:
+		return nil
+	}
 }
