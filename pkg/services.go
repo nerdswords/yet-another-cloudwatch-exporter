cb0038ea02474c3d97dd1a8686568659a7bd2930
Matthew Gumport <mbg@chronosphere.io>
2022-03-04 12:51:36 -0800

    services: add kinesis-analytics

--- pkg/services.go
+++ pkg/services.go
@@ -477,6 +477,9 @@ var (
 				aws.String(":stream/(?P<StreamName>[^/]+)"),
 			},
 		}, {
+			Namespace: "AWS/KinesisAnalytics",
+			Alias:     "kinesis-analytics",
+		}, {
 			Namespace: "AWS/Lambda",
 			Alias:     "lambda",
 			ResourceFilters: []*string{
