84ddaf6a0f0234746d01c28ccc5c6b91f962e460
Pablo Balbi <pablo.l.balbi@gmail.com>
2023-02-28 12:00:56 +0100

    import order

--- pkg/job/associator_test.go
+++ pkg/job/associator_test.go
@@ -2,11 +2,11 @@ package job
 
 import (
 	"fmt"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/cloudwatch"
 	"regexp"
 	"testing"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/cloudwatch"
 	"github.com/stretchr/testify/require"
 
 	"github.com/nerdswords/yet-another-cloudwatch-exporter/pkg/config"
