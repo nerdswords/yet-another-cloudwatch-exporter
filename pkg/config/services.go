7945691b46885693abffc556ffd89db4c4640b1b
Senne Theunis <senne.theunis@persgroep.net>
2023-05-12 14:20:31 +0200

    Improved regex

--- pkg/config/services.go
+++ pkg/config/services.go
@@ -480,7 +480,7 @@ var SupportedServices = serviceConfigs{
 			aws.String("mediaconvert"),
 		},
 		DimensionRegexps: []*regexp.Regexp{
-			regexp.MustCompile("(?P<Queue>.*:queues/.*)$"),
+			regexp.MustCompile("(?P<Queue>.*:.*:mediaconvert:.*:queues/.*)$"),
 		},
 	},
 	{
