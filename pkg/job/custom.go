d2b3bf2049bfa1c71a5d8c1c988946c6347c0e6c
Cristian Greco <cristian@regolo.cc>
2023-08-01 19:34:51 +0200

    Debug log GetMetricData partitions count

--- pkg/job/custom.go
+++ pkg/job/custom.go
@@ -37,6 +37,7 @@ func runCustomNamespaceJob(
 	maxMetricCount := metricsPerQuery
 	length := getMetricDataInputLength(job.Metrics)
 	partition := int(math.Ceil(float64(metricDataLength) / float64(maxMetricCount)))
+	logger.Debug("GetMetricData partitions", "total", partition)
 
 	wg.Add(partition)
 
