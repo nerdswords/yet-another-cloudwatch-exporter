fd4575e2de25969559e164d0e9bda6c26274f9eb
Pablo Balbi <pablo.l.balbi@gmail.com>
2024-06-18 17:44:33 -0300

    implement v1 client

--- pkg/clients/account/v1/client.go
+++ pkg/clients/account/v1/client.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/nerdswords/yet-another-cloudwatch-exporter/pkg/clients/account"
 
+	"github.com/aws/aws-sdk-go/service/iam"
+	"github.com/aws/aws-sdk-go/service/iam/iamiface"
 	"github.com/aws/aws-sdk-go/service/sts"
 	"github.com/aws/aws-sdk-go/service/sts/stsiface"
 
@@ -15,9 +17,10 @@ import (
 type client struct {
 	logger    logging.Logger
 	stsClient stsiface.STSAPI
+	iamClient iamiface.IAMAPI
 }
 
-func NewClient(logger logging.Logger, stsClient stsiface.STSAPI) account.Client {
+func NewClient(logger logging.Logger, stsClient stsiface.STSAPI, iamClient iamiface.IAMAPI) account.Client {
 	return &client{
 		logger:    logger,
 		stsClient: stsClient,
@@ -36,5 +39,19 @@ func (c client) GetAccount(ctx context.Context) (string, error) {
 }
 
 func (c client) GetAccountAlias(ctx context.Context) (string, error) {
-	return "", nil
+	acctAliasOut, err := c.iamClient.ListAccountAliases(&iam.ListAccountAliasesInput{})
+	if err != nil {
+		return "", err
+	}
+
+	possibleAccountAlias := ""
+
+	// Since a single account can only have one alias, and an authenticated SDK session corresponds to a single account,
+	// the output can have at most one alias.
+	// https://docs.aws.amazon.com/IAM/latest/APIReference/API_ListAccountAliases.html
+	if len(acctAliasOut.AccountAliases) > 0 {
+		possibleAccountAlias = *acctAliasOut.AccountAliases[0]
+	}
+
+	return possibleAccountAlias, nil
 }
