422216414d6530dacb2b3d50d09684c235ed23bb
Ruslan Mustaev <ruslan.mustaev@orionhealth.com>
2024-11-01 14:27:03 +1300

    Feature: Auto-discovery for Directory services #757

--- pkg/config/services.go
+++ pkg/config/services.go
@@ -244,6 +244,16 @@ var SupportedServices = serviceConfigs{
 		},
 	},
 	{
+		Namespace: "AWS/DirectoryService",
+		Alias:     "ds",
+		ResourceFilters: []*string{
+			aws.String("ds:directory"),
+		},
+		DimensionRegexps: []*regexp.Regexp{
+			regexp.MustCompile(":directory/(?P<DirectoryId>[^/]+)"),
+		},
+	},
+	{
 		Namespace: "AWS/DMS",
 		Alias:     "dms",
 		ResourceFilters: []*string{
